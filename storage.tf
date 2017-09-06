373ba2b1cae42cc2e0adf21680e8b027156a39a9
Ben Lucato <blucato@officeworks.com.au>
2017-09-06 23:11:06 +1000

    Fix things to the point where a VM can be started.

--- storage.tf
+++ storage.tf
@@ -43,13 +43,31 @@ resource "azurerm_network_interface" "test" {
   }
 }
 
+resource "azurerm_storage_account" "packer" {
+  name                = "vibratopacker"
+  resource_group_name = "${azurerm_resource_group.storage.name}"
+  location            = "westus2"
+  account_type        = "Standard_LRS"
+}
+
 resource "azurerm_storage_container" "packer" {
-  name = "images"
-  storage_account_name = "vibratopacker"
+  name = "system"
+  storage_account_name = "${azurerm_storage_account.packer.name}"
   resource_group_name = "${azurerm_resource_group.storage.name}"
   container_access_type = "private"
 }
 
+resource "azurerm_storage_blob" "image" {
+  name = "windows-2016-datacenter.vhd"
+
+  resource_group_name    = "${azurerm_resource_group.storage.name}"
+  storage_account_name   = "${azurerm_storage_account.packer.name}"
+  storage_container_name = "${azurerm_storage_container.packer.name}"
+
+  source_uri = "https://vibratopacker.blob.core.windows.net/system/Microsoft.Compute/Images/windows-2016-datacenter/packer-osDisk.674f0bf3-82b0-4b68-8f6c-58d77cab822b.vhd"
+}
+
+#https://vibratopacker.blob.core.windows.net/system/Microsoft.Compute/Images/windows-2016-datacenter/packer-osDisk.674f0bf3-82b0-4b68-8f6c-58d77cab822b.vhd
 resource "azurerm_virtual_machine" "test" {
   name                  = "dc1"
   location              = "westus2"
@@ -57,18 +75,17 @@ resource "azurerm_virtual_machine" "test" {
   network_interface_ids = ["${azurerm_network_interface.test.id}"]
   vm_size               = "Standard_A0"
 
-  delete_os_disk_on_termination = true
-  delete_data_disks_on_termination = true
-
   storage_os_disk {
     name          = "osdisk"
-    vhd_uri       = "https://vibratopacker.blob.core.windows.net/system/Microsoft.Compute/Images/windows-2016-datacenter/packer-osDisk.674f0bf3-82b0-4b68-8f6c-58d77cab822b.vhd"
+    image_uri = "${azurerm_storage_account.packer.primary_blob_endpoint}system/windows-2016-datacenter.vhd"
+    vhd_uri       = "${azurerm_storage_account.packer.primary_blob_endpoint}system/windows-2016-datacenter2.vhd"
     caching       = "ReadWrite"
     create_option = "FromImage"
+    os_type = "windows"
   }
 
   os_profile {
-    computer_name  = "hostname"
+    computer_name = "dc1"
     admin_username = "testadmin"
     admin_password = "Password1234!"
   }
